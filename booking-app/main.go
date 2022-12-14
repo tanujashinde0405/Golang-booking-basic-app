3acde3611198f6bdd2ee01fd3f41401b65a5ddb8
TanujaShinde04 <>
2022-12-14 05:45:19 +0000

    Initial commit

---
+++ booking-app/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+  fmt.Println("Welcome to our Go confernece booking application")
+  fmt.Println("Get your tickets here to attend")
+
+  var conferenceName = "Go Conference"
+  fmt.Println(conferenceName)
+  
+}
